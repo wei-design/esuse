414316ce4908dea3b72fc2b4f98f8991aa0580fd
wforguo <wforguo@qq.com>
2024-05-12 19:46:13 +0800

    feat: imageClipboard and imagePaste functions completed

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
+import pkg from './package.json'
+const { name } = pkg
 
 export default defineConfig({
     build: {
         lib: {
             entry: './lib/main.ts',
-            name: 'Counter',
-            fileName: 'counter'
+            name: name,
+            fileName: 'index'
         }
     }
 })
